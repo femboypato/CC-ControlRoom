60054533ff4ae5e56d62351f3355b05ac4ea0520
femboypato <174960651+femboypato@users.noreply.github.com>
2026-02-03 23:08:42 -0600

    add monitor api

--- lib/monitor.lua
+++ lib/monitor.lua
@@ -1,61 +1,85 @@
-local mon = peripheral.find("monitor")
-w, h = mon.getSize()
+os.loadAPI("lib/utils")
 
-function clear()
-    mon.clear()
-    mon.setBackgroundColor(colors.black)
-    mon.setTextColor(colors.white)
+Monitor = {
+    w = 0,
+    h = 0,
+    mon = nil
+}
+
+function Monitor:new(id)
+    local o = {}
+    setmetatable(o, self)
+    self.__index = self
+    o.mon = utils.getPeripheral("monitor", "monitor_"..id)
+    local w, h = mon.getSize()
+    o.w = w
+    o.h = h
+    return o
 end
 
-function drawText(x, y, text, textColor, bgColor)
+function Monitor:getW()
+    return self.w
+end
+
+function Monitor:getH()
+    return self.g
+end
+
+function Monitor:clear()
+    self.mon.clear()
+    self.mon.setBackgroundColor(colors.black)
+    self.mon.setTextColor(colors.white)
+end
+
+function Monitor:drawText(x, y, text, textColor, bgColor)
     textColor = textColor or colors.white
     bgColor = bgColor or colors.black
-    mon.setBackgroundColor(bgColor)
-    mon.setTextColor(textColor)
-    mon.setCursorPos(x,y)
-    mon.write(text)
+    self.mon.setBackgroundColor(bgColor)
+    self.mon.setTextColor(textColor)
+    self.mon.setCursorPos(x,y)
+    self.mon.write(text)
 end
 
-function drawTextLeft(x, y, text, textColor, bgColor)
-    drawText(x, y, text, textColor, bgColor)
+function Monitor:drawTextLeft(x, y, text, textColor, bgColor)
+    self:drawText(x, y, text, textColor, bgColor)
 end
 
-function drawTextCenter(paddingLeft, paddingRight, y, text, textColor, bgColor)
+function Monitor:drawTextCenter(paddingLeft, paddingRight, y, text, textColor, bgColor)
     local x = ((w-string.len(text))/2) + paddingLeft - paddingRight
-    drawText(x, y, text, textColor, bgColor)
+    self:drawText(x, y, text, textColor, bgColor)
 end
 
-function drawTextRight(padding, y, text, textColor, bgColor)
-    drawText(w-string.len(tostring(text))-padding, y, text, textColor, bgColor)
+function Monitor:drawTextRight(padding, y, text, textColor, bgColor)
+    self:drawText(w-string.len(tostring(text))-padding, y, text, textColor, bgColor)
 end
 
-function drawHLine(x, y, length, color)
+function Monitor:drawHLine(x, y, length, color)
     color = color or colors.white
     if length < 0 then
       length = 0
     end
 
-    mon.setBackgroundColor(color)
-    mon.setCursorPos(x,y)
-    mon.write(string.rep(" ", length))
+    self.mon.setBackgroundColor(color)
+    self.mon.setCursorPos(x,y)
+    self.mon.write(string.rep(" ", length))
 end
 
-function drawVLine(x, y, length, color)
+function Monitor:drawVLine(x, y, length, color)
     color = color or colors.white
     if length < 0 then
         length = 0
     end
 
-    mon.setBackgroundColor(color)
+    self.mon.setBackgroundColor(color)
     for i=y,length do
-        mon.setCursorPos(x, i)
-        mon.write(" ")
+        self.mon.setCursorPos(x, i)
+        self.mon.write(" ")
     end
 end
 
-function drawProgressBar(x, y, length, percent, barColor, bgColor)
+function Monitor:drawProgressBar(x, y, length, percent, barColor, bgColor)
     barColor = barColor or colors.blue
     bgColor = bgColor or colors.white
-    drawHLine(x, y, length, bgColor)
-    drawHLine(x, y, percent * length, barColor)
+    self:drawHLine(x, y, length, bgColor)
+    self:drawHLine(x, y, percent * length, barColor)
 end
--- program.lua
+++ program.lua
@@ -5,17 +5,18 @@ os.loadAPI("lib/monitor")
 -- monitor size 71 x 26
 local module1 = reactor_module.ReactorModule:new("Module 1", 1, 1)
 local module2 = reactor_module.ReactorModule:new("Module 2", 2, 2)
+local monitor = monitor.Monitor:new(1)
 
 function init()
     button.clearTable()
-    monitor.clear()
-    monitor.drawTextCenter(0, 0, monitor.h / 2 - 1, "Loading...")
+    monitor:clear()
+    monitor:drawTextCenter(0, 0, monitor:getW() / 2 - 1, "Loading...")
     print("Created "..module1:getName().." with target: "..module1:getTarget():getId())
     print("Created "..module2:getName().." with target: "..module2:getTarget():getId())
 
     -- cache initial data
     for i=1,20 do
-        monitor.drawProgressBar(monitor.w / 4, monitor.h / 2, monitor.w / 2, i / 20)
+        monitor:drawProgressBar(monitor:getW() / 4, monitor:getH() / 2, monitor:getW() / 2, i / 20)
         module1:refresh()
         module2:refresh()
         sleep(0.2)
@@ -24,14 +25,14 @@ end
 
 function screen()
     button.screen()
-    monitor.clear()
-    monitor.drawHLine(2, 2, monitor.w - 2)
-    monitor.drawHLine(2, monitor.h - 1, monitor.w - 2)
-    monitor.drawVLine(2, 2, monitor.h - 1)
-    monitor.drawVLine(monitor.w - 1, 2, monitor.h - 2)
-    monitor.drawTextCenter(0, 0, 2, " Reactor Control ", colors.red)
+    monitor:clear()
+    monitor:drawHLine(2, 2, monitor:getW() - 2)
+    monitor:drawHLine(2, monitor:getH() - 1, monitor:getW() - 2)
+    monitor:drawVLine(2, 2, monitor:getH() - 1)
+    monitor:drawVLine(monitor:getW() - 1, 2, monitor:getH() - 2)
+    monitor:drawTextCenter(0, 0, 2, " Reactor Control ", colors.red)
     
-    monitor.drawText(4, 4, "Module 1 - Generation: "
+    monitor:drawText(4, 4, "Module 1 - Generation: "
         ..module1:getRawTotal()
         .."su Usage: "
         ..module1:getRawUsage()
@@ -40,7 +41,7 @@ function screen()
         ..")"
     )
 
-    monitor.drawText(4, 5, "Module 2 - Generation: "
+    monitor:drawText(4, 5, "Module 2 - Generation: "
         ..module2:getRawTotal()
         .."su Usage: "
         ..module2:getRawUsage()
