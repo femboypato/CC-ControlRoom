d5c134d6334169df96e31d8c64f4ba81b1842527
AleBogino <boginoale@gmail.com>
2026-02-21 13:23:42 -0300

    removed shit debug logs
    it works! :)

--- lib/module_manager.lua
+++ lib/module_manager.lua
@@ -22,17 +22,6 @@ end
 function ModuleManager:toggleSignal(moduleIndex)
     print("Sending " .. "moduleManagerToggle_" .. tostring(moduleIndex))
     self.signalStates[moduleIndex] = not self.signalStates[moduleIndex]
-
-    local success, err = pcall(function()
-        local result = rednet.broadcast(self.signalStates[moduleIndex], "moduleManagerToggle_" .. tostring(moduleIndex))
-        if not result then
-            error("rednet.broadcast failed")
-        end
-    end)
-
-    if not success then
-        print("[Error] rednet.broadcast: " .. tostring(err))
-    end
-
-    print("Module " .. moduleIndex .. " is " .. (self.signalStates[moduleIndex] and "OFF" or "ON"))
+    rednet.broadcast(self.signalStates[moduleIndex], "moduleManagerToggle_" .. tostring(moduleIndex))
+    print("Sent " .. (self.signalStates[moduleIndex] and "OFF" or "ON") .. "to module" .. moduleIndex)
 end
