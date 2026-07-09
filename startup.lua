7e374a9bf46e3e7c21cd99e9064df21e869c81e8
AleBogino <boginoale@gmail.com>
2026-07-09 11:03:24 -0300

    add file download retrying on failure

--- startup.lua
+++ startup.lua
@@ -1,5 +1,22 @@
 function downloadFile(url, path)
-    local remoteStream = http.get(url)
+    local remoteStream = nil
+    local attempts = 0
+    local maxAttempts = 5
+
+    while remoteStream == nil and attempts < maxAttempts do
+        attempts = attempts + 1
+        remoteStream = http.get(url)
+        if remoteStream == nil then
+            print("Failed to download " .. url .. ". Attempt " .. attempts .. " of " .. maxAttempts .. ". Retrying..")
+            sleep(2)
+        end
+    end
+
+    if remoteStream == nil then
+        print("ERROR: Failed to download " .. path .. " after " .. attempts .. " attempts.")
+        return
+    end
+
     local remoteFile = remoteStream.readAll()
     local localFileWrite = fs.open(path, "w")
     if not fs.exists(path) then
